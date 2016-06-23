c18b117a7338c12a995b14af454f943edd359730
marin <marin.masic@shoutem.com>
2016-06-23 17:43:12 +0200

    Adjusting find for OSX

--- fastlane.sh
+++ fastlane.sh
@@ -1,11 +1,12 @@
-NOOFPROJECTS=$(find -path ./__MACOSX -prune -o -name '*.xcodeproj' -print | grep -o '\./.*/' | wc -l)
+NOOFPROJECTS=$(find . -path ./__MACOSX -prune -o -name '*.xcodeproj' -print | grep -o '\./.*/' | wc -l)
 
 if [ $NOOFPROJECTS != 1 ]; then
+  echo "Number of projects = " $NOOFPROJECTS ". Number of projects MUST BE ONE!"
   echo "Number of projects = " $NOOFPROJECTS ". Number of projects MUST BE ONE!" >> log.txt
   exit 1
 fi
 
-PROJPATH=$(find -path ./__MACOSX -prune -o -name '*.xcodeproj' -print | grep -o '\./.*/' | sed 's/\.\///g')
+PROJPATH=$(find . -path ./__MACOSX -prune -o -name '*.xcodeproj' -print | grep -o '\./.*/' | sed 's/\.\///g')
 cp fastlane_expect.sh $PROJPATH
 cd $PROJPATH
 
